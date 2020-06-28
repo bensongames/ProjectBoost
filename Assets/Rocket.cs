2677097924060109c699d50800198ec960426f96
Darren Benson <darren.benson@deskpoint.com>
2020-06-28 01:46:19 +0100

    Complete C# Unity Developer: 58 End of 3.2

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
@@ -10,6 +8,15 @@ public class Rocket : MonoBehaviour
     private AudioSource audioSource;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 1000f;
+
+    private enum State
+    {
+        Alive,
+        Dying,
+        Progressing
+    }
+
+    private State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
@@ -21,22 +28,42 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (state != State.Alive) return;
         Thrust();
         Rotate();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (state != State.Alive) return;
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                // Do nothing
+                break;
+            case "Finish":
+                state = State.Progressing;
+                Invoke("LoadNextLevel", 1f); //parameterise time
                 break;
             default:
-                print("Boom");
+                state = State.Dying;
+                audioSource.Stop();
+                Invoke("LoadFirstLevel", 1f); //parameterise time
                 break;
         }
     }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+        state = State.Alive;
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+        state = State.Alive;
+    }      
 
     private void Thrust()
     {
